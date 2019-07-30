9ee9c857f1c9fe5324b6a4faed78ffbe24eb86df
TinLVV <tin.lvv@3si.vn>
2019-07-30 14:51:02 +0700

    feat(add-header): Attribute add custom header info

--- src/Services/AudioBook/AudioBook.Api/Controllers/AudioBookController.cs
+++ src/Services/AudioBook/AudioBook.Api/Controllers/AudioBookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AudioBook.Api.Criterias;
+using AudioBook.Api.Filters;
 using AudioBook.API.Filters;
 using AudioBook.Core.Models;
 using AudioBook.Core.Models.Response;
@@ -20,6 +21,7 @@ namespace AudioBook.API.Controllers
     {
         [HttpGet("category/{categoryId}/audiobooks")]
         //[Audit]
+        [AddHeader("version", "1.1")]
         public async Task<ActionResult<PagedData<AudioBookModel>>> Gets(int categoryId, int page = 1)
         {
             var result = new PagedData<AudioBookModel>(new List<AudioBookModel>(), 0);
@@ -29,6 +31,7 @@ namespace AudioBook.API.Controllers
 
         [HttpGet("audiobooks")]
         //[Audit]
+        [AddHeader("version", "1.0")]
         public async Task<ActionResult<PagedData<AudioBookModel>>> Gets([FromQuery] GetAllAudioBookFilter filter)
         {
             var result = new PagedData<AudioBookModel>(new List<AudioBookModel>(), 0);
